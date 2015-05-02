694085814919f196f9574d211089251e6aa6d6ea
Luke Berry <contact@luke-berry.co.uk>
2015-05-02 11:50:18 +0100

    init commit

---
+++ CatSnapz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatSnapz
+//
+//  Created by Luke Berry on 02/05/2015.
+//  Copyright (c) 2015 Payzip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
